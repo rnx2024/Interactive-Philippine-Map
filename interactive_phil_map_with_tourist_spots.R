ffe51eb878c227b812fa1db9915b7e7317929a7c
rnx2024 <raniurbis@gmail.com>
2024-12-29 16:24:52 +0800

    Update interactive_phil_map_with_tourist_spots.R
    Signed-off-by: rnx2024 <raniurbis@gmail.com>

--- interactive_phil_map_with_tourist_spots.R
+++ interactive_phil_map_with_tourist_spots.R
@@ -4,11 +4,11 @@ library(htmltools)
 # Create a data frame with the locations, coordinates, and descriptions
 tourist_spots <- data.frame(
   place = c("Palawan", "Vigan", "Intramuros", "Siargao", "Boracay", "Chocolate Hills", "Camiguin", 
-            "Hundred Islands", "Banaue Rice Terraces", "Sagada", "Batanes", "Siquijor"),
+            "Hundred Islands", "Banaue Rice Terraces", "Sagada", "Batanes", "Siquijor", "Baler", "San Juan, La Union", "Baguio"),
   lat = c(9.8349, 17.5748, 14.5897, 9.8667, 11.9685, 9.8223, 9.1000, 
-          16.1925, 16.9310, 17.0830, 20.4474, 9.2086),
+          16.1925, 16.9310, 17.0830, 20.4474, 9.2086, 15.7480, 16.6956, 16.4023),
   lng = c(118.7388, 120.3873, 120.9738, 126.0500, 121.9248, 124.1756, 124.7000, 
-          120.0156, 121.0830, 120.9000, 121.9687, 123.5076),
+          120.0156, 121.0830, 120.9000, 121.9687, 123.5076, 121.5950, 120.3194, 120.5960),
   description = c(
     "Known for its stunning beaches,<br>crystal-clear waters, and the famous<br>Puerto Princesa Underground River.",
     "A well-preserved Spanish colonial town,<br>famous for its historic architecture and<br>cobblestone streets.",
@@ -21,7 +21,10 @@ tourist_spots <- data.frame(
     "Often called the 'Eighth Wonder of the World',<br>these ancient rice terraces are a breathtaking sight.",
     "Famous for its hanging coffins, caves,<br>and beautiful landscapes.",
     "A group of islands with stunning scenery<br>and a unique culture.",
-    "Known as the 'Island of Healing',<br>famous for its mystical charm and beautiful beaches."
+    "Known as the 'Island of Healing',<br>famous for its mystical charm and beautiful beaches.",
+    "Baler is famous for its surfing spots,<br>particularly Sabang Beach. It also boasts<br>historical sites like the Baler Church and<br>the Baler Hanging Bridge.",
+    "San Juan, La Union is known for its<br>excellent surfing conditions, with a laid-back<br>vibe and beautiful sunsets.",
+    "Baguio is renowned for its cool climate,<br>beautiful parks, vibrant arts scene,<br>and historical landmarks."
   )
 )
 
